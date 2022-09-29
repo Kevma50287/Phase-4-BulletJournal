cbaf32252293b4c7b5a4f3ac0d83aee655389a46
Kevin <magavakevin10@gmail.com>
2022-09-29 14:14:48 -0400

    Added login and profile routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   resources :journal_activity_joiners
   resources :activities
   resources :journal_entries
-  resources :users
+  resources :users, only: [:create]
+  post '/login', to: 'auth#create'
+  get '/profile', to: 'users#profile'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
