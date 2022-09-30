9fe5a8cfb44ece05bbb382b9c38914601bdf0b02
Kevin <magavakevin10@gmail.com>
2022-09-30 11:26:14 -0400

    Updated postgresql db.yml

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   resources :journal_activity_joiners
   resources :activities
   resources :journal_entries
+
+  #Create a user at 'users#create'
   resources :users, only: [:create]
+
+  
   post '/login', to: 'auth#create'
   get '/profile', to: 'users#profile'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
