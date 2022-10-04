cd641198db255a8f5f1a2ece5f2d4fbda3143c37
Kevin <magavakevin10@gmail.com>
2022-10-03 20:49:13 -0400

    Added Login

--- client/src/features/UserAuth/LoginPage.tsx
+++ client/src/features/UserAuth/LoginPage.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import axios from "axios"
 
 const LoginPage = () => {
   //States
@@ -10,6 +11,12 @@ const LoginPage = () => {
   const[loginCredentials,setLoginCredentials] = useState(initialLoginState)
   const navigate = useNavigate()
 
+  //UseEffects
+
+  useEffect (() => {
+    getUserProfile()
+  }, [])
+
   //Handlers
   const handleLoginCredentials = (e: React.ChangeEvent<HTMLInputElement>)=>{
     const{name,value} = e.target
@@ -19,12 +26,43 @@ const LoginPage = () => {
     )
   }
 
+  const getUserProfile = async () => {
+    const res = await axios.get('http://localhost:3000/profile', {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('jwt')}`
+      }
+    })
+    const data = res.data
+    return data
+  }
+
   const navigateToSignup = () => {
     navigate('/signup')
   }
 
-  const handleLoginSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleLoginSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
+    try {
+      const response = await axios.post('http://localhost:3000/login', {user:loginCredentials}, {
+        headers: {
+          'Content-Type':'application/json'
+        }
+      })
+      // If response is accepted then we store the token to local storage
+      if (response.status === 202){
+        const data = response.data
+        const jsonWebToken = data.jwt
+        const user = data.user
+        console.log(data)
+        localStorage.setItem("jwt", jsonWebToken)
+        navigate(`/user/${user.username}/`)
+      }
+    } catch (err:any) {
+      //If there's an error we throw a window alert
+      const response = err.response
+      const error = response.data.message
+      alert(error)
+    }
   }
 
   return (
@@ -54,8 +92,7 @@ const LoginPage = () => {
           <button type='submit'>Login</button>
           <h5 className="authButton" onClick={navigateToSignup}><u>Sign Up Here</u></h5>
         </form>
-    </>
-        
+    </>  
   )
 }
 
