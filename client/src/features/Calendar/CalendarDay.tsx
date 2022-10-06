d1d5d8473ae228bb03411d210ae01d0560e8e66f
Kevin <magavakevin10@gmail.com>
2022-10-06 19:10:34 -0400

    added circles and JE icon'

--- client/src/features/Calendar/CalendarDay.tsx
+++ client/src/features/Calendar/CalendarDay.tsx
@@ -10,7 +10,28 @@ interface CalendarProps {
 const CalendarDay = ({date, isInTheCurrentMonth} : CalendarProps) => {
   const currentDate = useAppSelector((state) => state.calendar.currentDate)
   const selectedDate = useAppSelector((state) => state.calendar.selectedDate)
+  const journal_entries = useAppSelector((state) => state.journal.journal_entries)
   const dispatch = useAppDispatch()
+  
+  const colorsTable:{[index:string]:string} = {
+    depressed: 'depressed-color',
+    sad: 'sad-color',
+    neutral:'neutral-color',
+    happy:'happy-color',
+    superHappy:'superHappy-color'
+  }
+
+  const entryForThisDay = journal_entries.filter((entry) => {
+    const dateObj = new Date(entry.date)
+    return dateObj.toDateString() === date.toDateString()
+  })
+
+  
+
+  const emotion = entryForThisDay[0]?.emotion
+  const entry = entryForThisDay[0]?.entry
+  
+  
 
 // TODO:On initial render, should fetch information from DB
 
@@ -26,6 +47,14 @@ const CalendarDay = ({date, isInTheCurrentMonth} : CalendarProps) => {
       {`${date.getDate()}`}
       {/* TODO: Need to add three icons (Mood, Activity, Journal).
       If a mood/activity/journal was recorded for that day include  */}
+        {emotion && 
+          <div className={`
+            ${emotion && `${colorsTable[emotion]}`}
+            mood-circle
+          `}>
+          </div>
+        }
+      
     </div>
   )
 }
